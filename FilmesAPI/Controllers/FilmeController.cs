91475bab9b3f0c2d6aacde4d4faa915828eeb1c1
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-17 15:25:11 -0300

    Criação de dois métodos que usam GET e inserção da variável id

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,13 +8,27 @@ namespace FilmesAPI.Controllers
     public class FilmeController: ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
-        {
+        {   
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+            Console.WriteLine(filme.Duracao);            
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 10)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
