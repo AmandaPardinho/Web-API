ea9e6264f44a9addc070942c536ec317b1440de3
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-18 18:30:03 -0300

    Adição do AutoMapper e do NewtonsoftJson

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -3,19 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//O builder de conexão abaixo só funciona no Program.cs se estivermos usando o Visual Studio 2022
+//ConnectionStrings => 
+////ConnectionStrings completa => ("Data Source=DESKTOP-MIHQT5G;Initial Catalog=Filmes_API;Integrated Security=False;User ID=sa;Password=amanda03;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"));
+////Alguns servidores só aceitam a string completa
+//abaixo, dois modos de fazer a conexão com o banco de dados:
+//builder.Services.AddDbContext<FilmeContext>(opts => opts.UseSqlServer
+//("Data Source=DESKTOP-MIHQT5G;Initial Catalog=Filmes_API;User ID=sa;Password=amanda03"));
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("FilmeConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//O builder de conexão abaixo só funciona no Program.cs se estivermos usando o Visual Studio 2022
-//ConnectionStrings => 
-//ConnectionStrings completa => ("Data Source=DESKTOP-MIHQT5G;Initial Catalog=Filmes_API;Integrated Security=False;User ID=sa;Password=amanda03;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"));
-//Alguns servidores só aceitam a string completa
-builder.Services.AddDbContext<FilmeContext>(opts => opts.UseSqlServer
-("Data Source=DESKTOP-MIHQT5G;Initial Catalog=Filmes_API;User ID=sa;Password=amanda03"));
 
 var app = builder.Build();
 
