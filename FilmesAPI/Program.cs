cf0ef46df9e0ce0aeeacc8526a42e494bbc795d1
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-19 10:44:36 -0300

    Inclusão do caminho para permitir a geração de documentação com o Swagger

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,7 +20,11 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
+  var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+  c.IncludeXmlComments(xmlPath);
+});
 
 
 var app = builder.Build();
