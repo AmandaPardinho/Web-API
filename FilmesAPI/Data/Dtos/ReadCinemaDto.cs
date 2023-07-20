f832778c50e24bbadcd80170148ef4a756e3e576
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:24:35 -0300

    Atualização do ReadDto de cinema

--- FilmesAPI/Data/Dtos/ReadCinemaDto.cs
+++ FilmesAPI/Data/Dtos/ReadCinemaDto.cs
@@ -3,9 +3,8 @@
     public class ReadCinemaDto
     {
         public int Id { get; set; }
-
         public string Nome { get; set; }
-
-        public ReadEnderecoDto ReadEnderecoDto { get; set; }
+        public ReadEnderecoDto Endereco { get; set; }
+        public ICollection<ReadSessaoDto> Sessoes { get; set; }
     }
 }
