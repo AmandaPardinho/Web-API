87807bd768008234c4532ea9c96a9aa852960954
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:23:22 -0300

    Atualização do filme context

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -13,5 +13,6 @@ namespace FilmesAPI.Data
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Sessao> Sessoes { get; set; }
     }
 }
