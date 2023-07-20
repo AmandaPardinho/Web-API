bfb1ca6bd3331c58730c32bb0c698b34590d37e4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:21:42 -0300

    Atualização do cinema profile

--- FilmesAPI/Profiles/CinemaProfile.cs
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -9,7 +9,9 @@ namespace FilmesAPI.Profiles
         public CinemaProfile()
         {
             CreateMap<CreateCinemaDto, Cinema>();
-            CreateMap<Cinema, ReadCinemaDto>();
+            CreateMap<Cinema, ReadCinemaDto>().
+                ForMember(cinemaDto => cinemaDto.Endereco, opt => opt.MapFrom(cinema => cinema.Endereco)).
+                ForMember(cinemaDto => cinemaDto.Sessoes, opt => opt.MapFrom(cinema => cinema.Sessoes));
             CreateMap<UpdateCinemaDto, Cinema>();
         }
     }
