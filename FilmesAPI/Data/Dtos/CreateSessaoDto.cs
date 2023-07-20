5552e5c9c2cc8e2c913206d7f1a2c5ce4cc21dc6
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:18:47 -0300

    Criado o CreateDto de sessao

---
+++ FilmesAPI/Data/Dtos/CreateSessaoDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateSessaoDto
+    {
+        [Required]
+        public int FilmeId { get; set; }
+        public int CinemaId { get; set; }
+    }
+}
