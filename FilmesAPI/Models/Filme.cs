0238847583eb7201fc663c89b50156a7d433593e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:22:06 -0300

    Atualização do model de filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilmesAPI.Models
 {
-    [Table("Filmes")]
     public class Filme
     {
         [Key]
@@ -20,5 +19,7 @@ namespace FilmesAPI.Models
         [Required]
         [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
+
+        public virtual ICollection<Sessao> Sessoes { get; set; }
     }
 }
