e81624ab2234f0313c56d27ac0f0ed650d2e0edb
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:21:20 -0300

    Atualização do filme profile

--- FilmesAPI/Profiles/FilmeProfile.cs
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -11,7 +11,8 @@ namespace FilmesAPI.Profiles
             CreateMap<CreateFilmeDto, Filme>();
             CreateMap<UpdateFilmeDto, Filme>();
             CreateMap<Filme, UpdateFilmeDto>();
-            CreateMap<Filme, ReadFilmeDto>();
+            CreateMap<Filme, ReadFilmeDto>().
+                ForMember(filmeDto => filmeDto.Sessoes, opt => opt.MapFrom(filme => filme.Sessoes));
         }
     }
 }
