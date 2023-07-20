d281166242a4d4bc20174073a4203e060ef30489
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-19 21:29:21 -0300

    Criação do Model do endereço

---
+++ FilmesAPI/Models/Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Endereco
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Logradouro { get; set; }
+
+        [Required]
+        public int Numero { get; set; }
+
+        public virtual Cinema Cinema { get; set; }
+    }
+}
