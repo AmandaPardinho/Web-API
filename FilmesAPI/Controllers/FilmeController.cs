e1edbd34f4c6666d0d122afa90440628cdbf70c7
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-20 15:25:02 -0300

    Atualização do controller de filme

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -40,7 +40,7 @@ namespace FilmesAPI.Controllers
         [HttpGet]
         public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 10)
         {
-            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
         }
 
         [HttpGet("{id}")]
