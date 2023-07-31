257c97dd20d06fd1de575a652819d3db296236f3
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-31 18:45:36 -0300

    Criação do model UF

---
+++ DesafioApiFilme/Models/UF.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Models
+{
+    public class UF
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string NomeUf { get; set; }
+    }
+}
