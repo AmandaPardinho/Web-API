7a9c8c3aa0934a6566a75ecbffab69be44aca3e1
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-31 18:46:47 -0300

    Criação do model Cidade

---
+++ DesafioApiFilme/Models/Cidade.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Models
+{
+    public class Cidade
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string NomeCidade { get; set; }
+
+        public string UfId { get; set; }
+
+        public int EnderecoId { get; set; }
+
+        //recupera instâncias
+        public virtual Endereco Endereco { get; set; }
+        public virtual UF Uf { get; set; }
+
+
+    }
+}
