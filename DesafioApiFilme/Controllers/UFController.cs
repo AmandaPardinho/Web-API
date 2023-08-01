67c81548ab704d65db12763862e1ef004d38faec
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-01 15:52:18 -0300

    Início do controller de uf

---
+++ DesafioApiFilme/Controllers/UFController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioApiFilme.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class UFController: ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+
+    }
+}
