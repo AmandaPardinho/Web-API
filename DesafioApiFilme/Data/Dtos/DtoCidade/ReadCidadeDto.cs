cb93889333c46adeb896f04d3e0b7436b8563721
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 09:31:08 -0300

    Criação do ReadCidadeDto

--- DesafioApiFilme/Data/Dtos/DtoCidade/ReadCidadeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoCidade/ReadCidadeDto.cs
@@ -1,6 +1,12 @@
-﻿namespace DesafioApiFilme.Data.Dtos.DtoCidade
+﻿using DesafioApiFilme.Data.Dtos.DtoEndereco;
+
+namespace DesafioApiFilme.Data.Dtos.DtoCidade
 {
     public class ReadCidadeDto
     {
+        public int CidadeId { get; set; }
+        public string NomeCidade { get; set; }
+        public string UfId { get; set; }
+        public ICollection<ReadEnderecoDto> Enderecos { get; set; }
     }
 }
