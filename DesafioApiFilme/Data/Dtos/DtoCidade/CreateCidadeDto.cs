abeee50406a114c56f8924419dc41d901f523342
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 09:31:34 -0300

    Criação do CreateCidadeDto

--- DesafioApiFilme/Data/Dtos/DtoCidade/CreateCidadeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoCidade/CreateCidadeDto.cs
@@ -1,6 +1,13 @@
-﻿namespace DesafioApiFilme.Data.Dtos.DtoCidade
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Data.Dtos.DtoCidade
 {
     public class CreateCidadeDto
     {
+        [Required]
+        public int CidadeId { get; set; }
+
+        [Required]
+        public string NomeCidade { get; set; }
     }
 }
