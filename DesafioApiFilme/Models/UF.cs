8de2f16d51c530bccfb01f214ac319bd9d3f4590
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 08:26:12 -0300

    Alteração na propriedade virtual

--- DesafioApiFilme/Models/UF.cs
+++ DesafioApiFilme/Models/UF.cs
@@ -11,7 +11,19 @@ namespace DesafioApiFilme.Models
         [Required]
         public string NomeUf { get; set; }
 
-        public int CidadeId { get; set; }
         public virtual ICollection<Cidade> Cidade { get; set; }
+
+
+        //usar tabela ASCII
+        //public UF()
+        //{
+        //    List<string> estado = new List<string>();
+
+        //    estado.Add("Sao Paulo");
+
+        //    List<char> siglaEstado = new List<char>();
+
+        //}
+
     }
 }
