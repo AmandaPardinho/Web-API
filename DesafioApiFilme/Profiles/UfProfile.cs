e397675b5d9ac450078a48e624c5da8afe44e057
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 08:17:07 -0300

    Criado o profile da UF

---
+++ DesafioApiFilme/Profiles/UfProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoUF;
+using DesafioApiFilme.Models;
+
+namespace DesafioApiFilme.Profiles
+{
+    public class UfProfile: Profile
+    {
+        public UfProfile()
+        {
+            CreateMap<CreateUfDto, UF>();
+            CreateMap<UF, ReadUfDto>()
+                .ForMember(ufDto => ufDto.Cidade, option => option.MapFrom(uf => uf.Cidade));
+            CreateMap<UpdateUfDto, UF>();
+        }
+    }
+}
