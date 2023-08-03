ab7b2a2d5deab1e12e06217dcd39ae62feb210b4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 09:28:18 -0300

    Criação do profile Cidade

--- DesafioApiFilme/Profiles/CidadeProfile.cs
+++ DesafioApiFilme/Profiles/CidadeProfile.cs
@@ -1,8 +1,18 @@
 ﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoCidade;
+using DesafioApiFilme.Models;
 
 namespace DesafioApiFilme.Profiles
 {
     public class CidadeProfile: Profile
     {
+        public CidadeProfile()
+        {
+            CreateMap<CreateCidadeDto, Cidade>();
+            CreateMap<Cidade, ReadCidadeDto>()
+                .ForMember(cidadeDto => cidadeDto.UfId, option => option.MapFrom(cidade => cidade.Uf))
+                .ForMember(cidadeDto => cidadeDto.Enderecos, option => option.MapFrom(cidade => cidade.Endereco));
+            CreateMap<UpdateCidadeDto, Cidade>();
+        }
     }
 }
