88fe72b392e50a9be8734ddfb180c76a5f20c2aa
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-03 09:30:36 -0300

    Criação do UpdateCidadeDto

--- DesafioApiFilme/Data/Dtos/DtoCidade/UpdateCidadeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoCidade/UpdateCidadeDto.cs
@@ -1,6 +1,13 @@
-﻿namespace DesafioApiFilme.Data.Dtos.DtoCidade
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Data.Dtos.DtoCidade
 {
     public class UpdateCidadeDto
     {
+        [Required]
+        public int CidadeId { get; set; }
+
+        [Required]
+        public string NomeCidade { get; set; }
     }
 }
