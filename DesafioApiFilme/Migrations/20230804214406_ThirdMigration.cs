eb238b5611b8bbec06b467eba3953c4ce5034295
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-04 18:55:50 -0300

    Terceira migration para atualizar os nomes dos campos das tabelas

---
+++ DesafioApiFilme/Migrations/20230804214406_ThirdMigration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioApiFilme.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomeUf",
+                table: "Ufs",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "SiglaUfId",
+                table: "Ufs",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "NomeCidade",
+                table: "Cidades",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "IdCidade",
+                table: "Cidades",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Ufs",
+                newName: "NomeUf");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Ufs",
+                newName: "SiglaUfId");
+
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Cidades",
+                newName: "NomeCidade");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Cidades",
+                newName: "IdCidade");
+        }
+    }
+}
