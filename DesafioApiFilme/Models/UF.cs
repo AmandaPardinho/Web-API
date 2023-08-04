93adbf9c8ebf13696c639acf5e8b663e5ad8cdc3
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-04 19:22:00 -0300

    Atualização no nome dos campos

--- DesafioApiFilme/Models/UF.cs
+++ DesafioApiFilme/Models/UF.cs
@@ -6,21 +6,12 @@ namespace DesafioApiFilme.Models
     {
         [Key]
         [Required]
-        public string UfId { get; set; }
+        public string Id { get; set; }
 
         [Required]
-        public string NomeUf { get; set; }
+        public string Nome { get; set; }
 
+        public int CidadeId { get; set; }
         public virtual ICollection<Cidade> Cidade { get; set; }
-
-        //public UF()
-        //{
-        //    var id = UfId;
-        //    var idMaiusculo = id.ToUpper();
-
-        //    var nome = NomeUf;
-        //    var nomeMaiusculo = nome.ToUpper();
-
-        //}
     }
 }
