6afc6f66d73e780b52af072724ab60ffa52a2388
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 15:44:58 -0300

    Criação do model cliente

---
+++ DesafioApiFilme/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+
+        [Required]
+        public int Idade { get; set; }
+
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+
+        public int IngressoId { get; set; }
+        public virtual Ingresso Ingresso { get; set; }
+    }
+}
