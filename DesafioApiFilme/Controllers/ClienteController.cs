d24a71a0aab61c914cadbf1936a7d3eaeb9f96ce
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 19:25:41 -0300

    Criação do CRUD

--- DesafioApiFilme/Controllers/ClienteController.cs
+++ DesafioApiFilme/Controllers/ClienteController.cs
@@ -1,6 +1,68 @@
-﻿namespace DesafioApiFilme.Controllers
+﻿using AutoMapper;
+using DesafioApiFilme.Data;
+using DesafioApiFilme.Data.Dtos.DtoCliente;
+using DesafioApiFilme.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioApiFilme.Controllers
 {
-    public class ClienteController
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
     {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public ClienteController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AdicionaCliente([FromBody] CreateClienteDto clienteDto)
+        {
+            Cliente cliente = _mapper.Map<Cliente>(clienteDto);
+            _context.Clientes.Add(cliente);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaClientePorId), new { id = cliente.Id }, cliente);
+        }
+
+        [HttpGet]
+        public IEnumerable<ReadClienteDto> RecuperaClientes()
+        {
+            return _mapper.Map<List<ReadClienteDto>>(_context.Clientes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaClientePorId(int id)
+        {
+            var cliente = _context.Clientes.FirstOrDefault(cliente => cliente.Id == id);
+            if (cliente == null) return NotFound();
+
+            var clienteDto = _mapper.Map<ReadClienteDto>(cliente);
+            return Ok(clienteDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaCliente(int id, [FromBody] UpdateClienteDto clienteDto)
+        {
+            var cliente = _context.Clientes.FirstOrDefault(cliente => cliente.Id == id);
+            if(cliente == null) return NotFound();
+            _mapper.Map(clienteDto, cliente);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaCliente(int id)
+        {
+            var cliente = _context.Clientes.FirstOrDefault(cliente => cliente.Id == id);
+            if(cliente == null) return NotFound();
+            _context.Remove(cliente);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
