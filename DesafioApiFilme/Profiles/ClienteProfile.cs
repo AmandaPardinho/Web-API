580b7a7b5b35453ae271cfde2671f0663207406e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 15:44:10 -0300

    criação do profile cliente

---
+++ DesafioApiFilme/Profiles/ClienteProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoCliente;
+using DesafioApiFilme.Models;
+
+namespace DesafioApiFilme.Profiles
+{
+    public class ClienteProfile: Profile
+    {
+        public ClienteProfile()
+        {
+            CreateMap<CreateClienteDto, Cliente>();
+            CreateMap<Cliente, ReadClienteDto>()
+                .ForMember(clienteDto => clienteDto.EnderecoId, opt => opt.MapFrom(cliente => cliente.Enderecos))
+                .ForMember(clienteDto => clienteDto.IngressoId, opt => opt.MapFrom(cliente =>
+                cliente.Ingresso));
+            CreateMap<UpdateClienteDto, Cliente>();
+        }
+    }
+}
