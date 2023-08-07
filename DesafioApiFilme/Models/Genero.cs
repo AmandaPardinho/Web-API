2039a6c71a1724f77507fd1e09b79478dd0fef1e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 17:09:05 -0300

    criação do model de genero

---
+++ DesafioApiFilme/Models/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Models
+{
+    public class Genero
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public int FilmeId { get; set; }
+        public virtual Filme Filmes { get; set; }
+    }
+}
