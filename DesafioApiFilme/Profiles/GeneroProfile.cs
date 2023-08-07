9d76f5ef3e626d267f598e5e3c8d2e69b3ce138c
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 17:08:43 -0300

    Criação do profile de genero

---
+++ DesafioApiFilme/Profiles/GeneroProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoGenero;
+using DesafioApiFilme.Models;
+
+namespace DesafioApiFilme.Profiles
+{
+    public class GeneroProfile: Profile
+    {
+        public GeneroProfile()
+        {
+            CreateMap<CreateGeneroDto, Genero>();
+            CreateMap<Genero, ReadGeneroDto>()
+                .ForMember(generoDto => generoDto.Filme, opt => opt.MapFrom(genero => genero.Filmes));
+            CreateMap<UpdateGeneroDto, Genero>();
+
+        }
+    }
+}
