1e7aed03aac8328ae9f89a836638611ae942701b
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 16:38:34 -0300

    Adição da propriedade horário

--- DesafioApiFilme/Data/Dtos/DtoSessao/CreateSessaoDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoSessao/CreateSessaoDto.cs
@@ -5,7 +5,11 @@ namespace DesafioApiFilme.Data.Dtos.DtoSessao
     public class CreateSessaoDto
     {
         [Required]
+        public TimeOnly Horario { get; set; }
+
+        [Required]
         public int FilmeId { get; set; }
+
         public int CinemaId { get; set; }
     }
 }
