897957d9461f4a919248779583b741be94768a82
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 15:44:30 -0300

    Criação do model ingresso

---
+++ DesafioApiFilme/Models/Ingresso.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioApiFilme.Models
+{
+    public class Ingresso
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public int SessaoId { get; set; }
+        public virtual Sessao Sessao { get; set; }
+
+        public int ClienteId { get; set; }
+        public virtual Cliente Cliente { get; set; }
+    }
+}
