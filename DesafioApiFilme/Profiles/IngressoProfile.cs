243868731d9b84fd9be516f121b4dfb32275a849
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-07 15:43:17 -0300

    Criação do ingresso profile

---
+++ DesafioApiFilme/Profiles/IngressoProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoIngresso;
+using DesafioApiFilme.Models;
+
+namespace DesafioApiFilme.Profiles
+{
+    public class IngressoProfile: Profile
+    {
+        public IngressoProfile()
+        {
+            CreateMap<CreateIngressoDto, Ingresso>();
+            CreateMap<Ingresso, ReadIngressoDto>()
+                .ForMember(ingressoDto => ingressoDto.ClienteId, opt => opt.MapFrom(ingresso => ingresso.Cliente))
+                .ForMember(ingressoDto => ingressoDto.SessaoId, opt => opt.MapFrom(ingresso => ingresso.Sessao));
+            CreateMap<UpdateIngressoDto, Ingresso>();
+        }
+    }
+}
