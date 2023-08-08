03e42eb4065defd31af888a5afd32548b8ef0351
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-08 12:45:17 -0300

    Inclusão da propriedade GeneroId

--- DesafioApiFilme/Data/Dtos/DtoFilme/ReadFilmeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoFilme/ReadFilmeDto.cs
@@ -5,9 +5,9 @@ namespace DesafioApiFilme.Data.Dtos.DtoFilme
     public class ReadFilmeDto
     {
         public string Titulo { get; set; }
-        public string Genero { get; set; }
         public int Duracao { get; set; }
         public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+        public int GeneroId { get; set; }
         public ICollection<ReadSessaoDto> Sessoes { get; set; }
     }
 }
