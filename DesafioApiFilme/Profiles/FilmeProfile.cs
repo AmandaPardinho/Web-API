47c73c72db37b9eb2d6501024d63e8694b91d1b2
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-08 12:36:31 -0300

    Mapeamento do genero

--- DesafioApiFilme/Profiles/FilmeProfile.cs
+++ DesafioApiFilme/Profiles/FilmeProfile.cs
@@ -12,7 +12,8 @@ namespace DesafioApiFilme.Profiles
             CreateMap<UpdateFilmeDto, Filme>();
             CreateMap<Filme, UpdateFilmeDto>();
             CreateMap<Filme, ReadFilmeDto>()
-                .ForMember(filmeDto => filmeDto.Sessoes, opt => opt.MapFrom(filme => filme.Sessoes));
+                .ForMember(filmeDto => filmeDto.Sessoes, opt => opt.MapFrom(filme => filme.Sessoes))
+                .ForMember(filmeDto => filmeDto.GeneroId, opt => opt.MapFrom(filme => filme.Generos));
         }
     }
 }
