0168826b745f0e129f70f3e7dd2520ac21f0debe
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-08 10:56:35 -0300

    Alteração no tipo do atributo horario

--- DesafioApiFilme/Data/Dtos/DtoSessao/UpdateSessaoDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoSessao/UpdateSessaoDto.cs
@@ -1,8 +1,10 @@
-﻿namespace DesafioApiFilme.Data.Dtos.DtoSessao
+﻿using System.Data.SqlTypes;
+
+namespace DesafioApiFilme.Data.Dtos.DtoSessao
 {
     public class UpdateSessaoDto
     {
-        public TimeOnly Horario { get; set; }
+        public SqlDateTime Horario { get; set; }
         public int FilmeId { get; set; }
     }
 }
