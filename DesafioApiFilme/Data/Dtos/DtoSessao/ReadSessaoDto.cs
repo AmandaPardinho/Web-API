3824a179ad8e5e691d49ed3a608500c5fc25a851
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-08 12:42:32 -0300

    Alteração no tipo do atributo

--- DesafioApiFilme/Data/Dtos/DtoSessao/ReadSessaoDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoSessao/ReadSessaoDto.cs
@@ -5,7 +5,7 @@ namespace DesafioApiFilme.Data.Dtos.DtoSessao
     public class ReadSessaoDto
     {
         public int Id { get; set; }
-        public SqlDateTime Horario { get; set; }
+        public DateTime Horario { get; set; }
         public int FilmeId { get; set; }
         public int CinemaId { get; set; }
     }
