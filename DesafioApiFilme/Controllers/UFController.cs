d5b6bde8ffabb51d2c8e6c193ea60cf5298d77be
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-09 17:15:37 -0300

    Refatoração do método GET

--- DesafioApiFilme/Controllers/UFController.cs
+++ DesafioApiFilme/Controllers/UFController.cs
@@ -30,19 +30,9 @@ namespace DesafioApiFilme.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<ReadUfDto> RecuperaUf(/*[FromQuery] int skip = 0, [FromQuery] int take = 1, [FromQuery]*/ string? nomeCidade = null)
+        public IEnumerable<ReadUfDto> RecuperaUf()
         {
-            if(nomeCidade == null)
-            {
-                return _mapper.Map<List<ReadUfDto>>(_context.Ufs.ToList());
-                //return _mapper.Map<List<ReadUfDto>>(_context.Ufs.Skip(skip).Take(take).ToList());
-            }
-            return _mapper.Map<List<ReadUfDto>>(_context.Ufs
-                .Where(uf => uf.Cidade
-                .Any(cidade => cidade.Nome == nomeCidade)));
-            //return _mapper.Map<List<ReadUfDto>>(_context.Ufs.Skip(skip).Take(take)
-                //.Where(uf => uf.Cidade
-                //.Any(cidade => cidade.NomeCidade == nomeCidade)));
+            return _mapper.Map<List<ReadUfDto>>(_context.Ufs);
         }
 
         [HttpGet("{id}")]
