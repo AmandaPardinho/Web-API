eb81d46ba84712e95ef81e5bfcc4e258267f10cc
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-09 16:30:29 -0300

    Retirado mapeamento cliente-endereco e cliente ingresso

--- DesafioApiFilme/Profiles/ClienteProfile.cs
+++ DesafioApiFilme/Profiles/ClienteProfile.cs
@@ -7,12 +7,10 @@ namespace DesafioApiFilme.Profiles
     public class ClienteProfile: Profile
     {
         public ClienteProfile()
+
         {
             CreateMap<CreateClienteDto, Cliente>();
-            CreateMap<Cliente, ReadClienteDto>()
-                .ForMember(clienteDto => clienteDto.EnderecoId, opt => opt.MapFrom(cliente => cliente.Enderecos))
-                .ForMember(clienteDto => clienteDto.IngressoId, opt => opt.MapFrom(cliente =>
-                cliente.Ingressos));
+            CreateMap<Cliente, ReadClienteDto>();
             CreateMap<UpdateClienteDto, Cliente>();
         }
     }
