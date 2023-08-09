9f34a38f22da68a887507878d1feec0539b0542e
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-09 17:08:34 -0300

    Retirada das dataAnnotations

--- DesafioApiFilme/Data/Dtos/DtoFilme/UpdateFilmeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoFilme/UpdateFilmeDto.cs
@@ -4,11 +4,8 @@ namespace DesafioApiFilme.Data.Dtos.DtoFilme
 {
     public class UpdateFilmeDto
     {
-        [Required(ErrorMessage = "O título do filme é obrigatório")]
         public string Titulo { get; set; }
 
-        [Required]
-        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
