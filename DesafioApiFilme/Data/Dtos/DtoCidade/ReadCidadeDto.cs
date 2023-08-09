ba48ba4854f09cabd6526781f16d690ca840d777
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-09 17:14:06 -0300

    Alteração no nome da propriedade de siglaUfId para UfId

--- DesafioApiFilme/Data/Dtos/DtoCidade/ReadCidadeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoCidade/ReadCidadeDto.cs
@@ -6,7 +6,7 @@ namespace DesafioApiFilme.Data.Dtos.DtoCidade
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string SiglaUfId { get; set; }
+        public string UfId { get; set; }
         public ICollection<ReadEnderecoDto> Enderecos { get; set; }
     }
 }
