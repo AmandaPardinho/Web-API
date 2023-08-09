c4f8922e433375e15e3847cce95f543b6d9292cc
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-09 17:09:27 -0300

    Inclisão da propriedade GeneroId

--- DesafioApiFilme/Data/Dtos/DtoFilme/CreateFilmeDto.cs
+++ DesafioApiFilme/Data/Dtos/DtoFilme/CreateFilmeDto.cs
@@ -10,5 +10,7 @@ namespace DesafioApiFilme.Data.Dtos.DtoFilme
         [Required]
         [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
+
+        public int GeneroId { get; set; }
     }
 }
