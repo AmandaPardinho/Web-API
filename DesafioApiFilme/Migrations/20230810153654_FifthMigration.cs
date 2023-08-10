981cd21546b956497a465ff666aa00e7919760c2
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-10 17:31:23 -0300

    Migration com a correção da relação filme-genero

---
+++ DesafioApiFilme/Migrations/20230810153654_FifthMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioApiFilme.Migrations
+{
+    /// <inheritdoc />
+    public partial class FifthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Filmes_GeneroId",
+                table: "Filmes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filmes_GeneroId",
+                table: "Filmes",
+                column: "GeneroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Filmes_GeneroId",
+                table: "Filmes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filmes_GeneroId",
+                table: "Filmes",
+                column: "GeneroId",
+                unique: true);
+        }
+    }
+}
