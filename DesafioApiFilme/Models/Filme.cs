1583babf0893b2b73944ead42bbe80667d89b8cd
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-10 17:34:02 -0300

    Nova definição de genero

--- DesafioApiFilme/Models/Filme.cs
+++ DesafioApiFilme/Models/Filme.cs
@@ -15,7 +15,7 @@ namespace DesafioApiFilme.Models
         [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
 
-        public int? GeneroId { get; set; }
+        public int GeneroId { get; set; }
         public virtual Genero Genero { get; set; }
 
         public virtual ICollection<Sessao> Sessoes { get; set; }
