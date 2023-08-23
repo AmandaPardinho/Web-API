27bf332f6b35cd8209f597f0f58fb758c1fb2573
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-08-23 20:15:26 -0300

    Alterações nas chamadas das funções

--- SQL/procedure.sql
+++ SQL/procedure.sql
@@ -1,50 +1,36 @@
 --USE API_Desafio_Cinema;
 
+
 --STORED PROCEDURE
 CREATE OR ALTER PROCEDURE ListaInfosVenda (
 @nome VARCHAR(50),
 @idade INT ,
 @cinema VARCHAR(100),
 @filme VARCHAR(50),
-@genero VARCHAR(25),
-@horario datetime2,
-@data datetime2,
-@duracao datetime2
+@genero VARCHAR(25)
 )
 AS
 BEGIN
 	--Query com as informações que serão retornadas
+	
 	SELECT 
 		UPPER(Cli.Nome) AS 'Nome',
 		UPPER(Cli.Sexo) AS 'IdentidadeGênero',
-		Cli.Idade AS 'Idade', 
-			CASE WHEN Idade <= 12 THEN 'CRIANÇA'
-			WHEN Idade BETWEEN 13 AND 19 THEN 'ADOLESCENTE'
-			WHEN Idade BETWEEN 20 AND 59 THEN 'ADULTO'
-			ELSE 'IDOSO' 
-			END AS  'Faixa Etária' ,
+		Cli.Idade AS 'Idade',
+		dbo.Func_Faixa_Etaria(Cli.Idade) AS  'Faixa Etária',
 		UPPER(Ct.Nome) AS 'Cidade',
 		UPPER(Est.Nome) AS 'Estado',
 		UPPER(Ct.UfId) AS 'Sigla',
 		UPPER(Cn.Nome) AS 'Cinema',
 		UPPER(CnCt.Nome) AS 'CidadeCinema',
 		I.Id AS 'Identificador',
-		/*CONCAT(
-		DATEPART(DAY, S.Horario),
-		'/',
-		DATEPART(MONTH, S.Horario),
-		'/',
-		DATEPART(YEAR, S.Horario) 
-		)*/
-		@data AS 'Data',
-		/*FORMAT(CAST(S.Horario AS datetime2), N'HH:mm') AS*/ 
-		@horario AS 'Horário',
+		(SELECT * FROM dbo.Func_Extrai_Data(Cli.Nome)) AS 'Data',
+		--dbo.Func_Formata_Horario(Clientes.Nome)  AS 'Horário',
 		UPPER(F.Titulo) AS 'Filme',
 		UPPER(G.Nome) AS 'Genero',
-		F.Duracao AS 'Duração',
-			/*FORMAT(
-				CAST(DATEADD(MINUTE, +F.Duracao, S.Horario) AS datetime2), N'HH:mm'
-			)*/@duracao AS 'FimSessão'
+		F.Duracao AS 'Duração',			
+		--dbo.Func_Duracao_Sessao(Clientes.Nome) AS 'FimSessão'
+		dbo.Fim_Sessao(S.Horario, F.Duracao) AS 'FimSessão'
 	FROM Clientes AS Cli
 		INNER JOIN Enderecos AS E ON Cli.EnderecoId = E.Id 
 		INNER JOIN Cidades AS Ct ON Ct.Id = E.CidadeId 
@@ -56,22 +42,17 @@ BEGIN
 				LEFT JOIN Cidades AS CnCt ON CnCt.Id = CnE.CidadeId
 		INNER JOIN Filmes AS F ON F.Id = S.FilmeId 
 		INNER JOIN Generos AS G ON G.Id = F.GeneroId
-	WHERE
-		(@data = (SELECT * FROM dbo.Func_Extrai_Data())) AND
-		(@horario = (SELECT * FROM dbo.Func_Formata_Horario())) AND
-		(@duracao = (SELECT * FROM dbo.Func_Duracao_Sessao())) AND
+	WHERE		
 		(Cli.Nome LIKE '%' + @nome + '%' OR @nome IS NULL) AND
 		(Cli.Idade = @idade OR @idade = 0) AND
 		(Cn.Nome LIKE '%' + @cinema + '%' OR @cinema IS NULL) AND
 		(F.Titulo LIKE '%' + @filme + '%' OR @filme IS NULL) AND
 		(G.Nome LIKE '%' + @genero + '%' OR @genero IS NULL) 		
 	ORDER BY S.Horario ASC;
+
+
 END
 GO
 
-EXECUTE ListaInfosVenda 
-	@nome = NULL, 
-	@idade = NULL, 
-	@cinema = NULL, 
-	@filme = NULL, 
-	@genero = NULL;
+
+
