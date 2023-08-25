427db8adab5f2db352bf563dcc1b9d5cf324632a
AmandaPardinho <amanda.pardinho@performait.com>
2023-08-25 16:35:16 -0300

    Criação do profile da procedure

---
+++ DesafioApiFilme/Profiles/ProcProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DesafioApiFilme.Data.Dtos.DtoProc;
+using DesafioApiFilme.Models;
+
+namespace DesafioApiFilme.Profiles
+{
+    public class ProcProfile: Profile
+    {
+        public ProcProfile()
+        {
+            CreateMap<CreateProcDto, Proc>();
+            CreateMap<Proc, ReadProcDto>();
+            CreateMap<UpdateProcDto, Proc>();
+        }
+    }
+}
