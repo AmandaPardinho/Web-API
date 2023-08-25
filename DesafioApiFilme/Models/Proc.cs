5d4174c6c421411ccf3a76899a93e08f5a2ed70f
AmandaPardinho <amanda.pardinho@performait.com>
2023-08-25 16:27:19 -0300

    Criação do model da procedure

---
+++ DesafioApiFilme/Models/Proc.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioApiFilme.Models
+{
+    [Keyless]
+    public class Proc
+    {
+        public string? Nome { get; set; }        
+        public int? Idade { get; set; }
+        public virtual IEnumerable<Cliente> Cliente { get; set; }
+
+        public string? Cinema { get; set; }
+        public virtual IEnumerable<Cinema> Cinemas { get; set; }
+
+        public string? Filme { get; set; }
+        public virtual IEnumerable<Filme> Filmes { get; set; }
+
+        public string? Genero { get; set; }
+        public virtual IEnumerable<Genero> Generos { get; set; }
+    }
+}
